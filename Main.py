aa6e66383ec53833c41f35b5d663244fcab41e20
wojciech1871 <wojciech1871@gmail.com>
2020-01-31 13:51:05 +0100

    DFO algorithm: 2 versions.

--- Main.py
+++ Main.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from Solvers.IterativeHardThresholding import iterative_hard_thresholding
+from Solvers.DiscreteFirstOrder import discrete_first_order, discrete_first_order_modified
 from DatasetGenerator import dataset_generator
 
 
-def plot_loss(hard_thresholding_hist, nesterov_hist, sample_num):
+def plot_loss(hard_thresholding_hist, dfo_hist, dfo_mod_hist, sample_num):
     plt.plot(hard_thresholding_hist, label='Loss for hard thresholding algorithm')
-    plt.plot(nesterov_hist, label="Nesterov's algorithm")  # TODO: change label
+    plt.plot(dfo_hist, label="DFO algorithm")
+    plt.plot(dfo_mod_hist, label="DFO modified algorithm")
     plt.ylim(ymin=0)
     # plt.yscale("log")
     plt.title(f'Loss for sample {sample_num}')
@@ -31,9 +33,9 @@ def main():
         iter_limit = 100
         eps = 0.001
         _, ith_hist = iterative_hard_thresholding(X, y, init_beta, L0_thresh, iter_limit, eps)
-        #TODO: Call Nesterov's algorithm
-        nesterov_hist_mock = np.random.rand(len(ith_hist)) * np.mean(ith_hist)
-        plot_loss(ith_hist, nesterov_hist_mock, i + 1)
+        _, dfo_hist = discrete_first_order(X, y, init_beta, L0_thresh, iter_limit, eps)
+        _, dfo_mod_hist = discrete_first_order_modified(X, y, init_beta, L0_thresh, iter_limit, eps)
+        plot_loss(ith_hist, dfo_hist, dfo_mod_hist, i + 1)
 
 
 if __name__ == "__main__":
---
+++ Solvers/DiscreteFirstOrder.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def square_euclidean(vec):
+    return np.inner(vec, vec)
+
+
+def discrete_first_order_modified(X, y, beta_init, dim_thresh, iter_limit, eps):
+    X_t = np.transpose(X)
+    beta = beta_init
+    loss_hist = []
+    L = 100 * max(np.linalg.eigvals(np.dot(X_t, X)))
+    for i in range(iter_limit):
+        H_operator_arg = beta + L * np.dot(X_t, y - np.dot(X, beta))
+        eta = H_operator(H_operator_arg, dim_thresh)
+        etabetax = np.dot(X, eta - beta)
+        lamb = np.dot(np.transpose(etabetax), y - np.dot(X, beta)) / square_euclidean(etabetax)
+        beta = lamb * eta + (1 - lamb) * beta
+        diff = y - np.dot(X, beta)
+        loss = square_euclidean(diff)
+        loss_hist.append(loss)
+        if loss < eps:
+            break
+    return beta, loss_hist
+
+
+def discrete_first_order(X, y, beta_init, dim_thresh, iter_limit, eps):
+    X_t = np.transpose(X)
+    beta = beta_init
+    loss_hist = []
+    L = 100 * max(np.linalg.eigvals(np.dot(X_t, X)))
+    for i in range(iter_limit):
+        H_operator_arg = beta + L * np.dot(X_t, y - np.dot(X, beta))
+        beta = H_operator(H_operator_arg, dim_thresh)
+        diff = y - np.dot(X, beta)
+        loss = square_euclidean(diff)
+        loss_hist.append(loss)
+        if loss < eps:
+            break
+    return beta, loss_hist
+
+
+def H_operator(vec, dim_thresh):
+    #  Select dim_thresh largest elements of vec
+    order = np.argsort(-vec)  # "-" because argsort sorts in ascending order
+    res = np.zeros_like(vec)
+    for i in range(dim_thresh):
+        curr_idx = order[i]
+        res[curr_idx] = vec[curr_idx]
+    return res
