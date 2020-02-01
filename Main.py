00139c1c8a3faba0ad907f5970a2c9cf0118cb5b
MichalTurski <michal.antoni.turski@gmail.com>
2020-02-01 10:32:16 +0100

    Plot iterations number in function of threshold.

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from Solvers.IterativeHardThresholding import iterative_hard_thresholding
 from Solvers.DiscreteFirstOrder import discrete_first_order, discrete_first_order_modified
@@ -19,7 +20,7 @@ def plot_loss(hard_thresholding_hist, dfo_hist, dfo_mod_hist, sample_num):
     plt.show()
 
 
-def main():
+def plot_losses():
     for i, (X, y) in enumerate(dataset_generator()):
         init_beta = np.random.rand(X.shape[1])
         L0_thresh = int(init_beta.shape[0]/2)
@@ -31,5 +32,39 @@ def main():
         plot_loss(ith_hist, dfo_hist, dfo_mod_hist, i + 1)
 
 
+def plot_itrations():
+    for i, (X, y) in enumerate(dataset_generator()):
+        init_beta = np.random.rand(X.shape[1])
+        iter_limit = 1000
+        eps = 0.0001
+        ith_iters_list = []
+        dfo_iters_list = []
+        dfo_mod_iters_list = []
+        for j in range(50):
+            ith_iters = []
+            dfo_iters = []
+            dfo_mod_iters = []
+            for L0_thresh in range(1, int(init_beta.shape[0])):
+                _, ith_hist = iterative_hard_thresholding(X, y, init_beta, L0_thresh, iter_limit, eps)
+                ith_iters.append(len(ith_hist))
+                _, dfo_hist = discrete_first_order(X, y, init_beta, L0_thresh, iter_limit, eps)
+                dfo_iters.append(len(dfo_hist))
+                _, dfo_mod_hist = discrete_first_order_modified(X, y, init_beta, L0_thresh, iter_limit, eps)
+                dfo_mod_iters.append(len(dfo_mod_hist))
+            ith_iters_list.append(ith_iters)
+            dfo_iters_list.append(dfo_iters)
+            dfo_mod_iters_list.append(dfo_mod_iters)
+        ith_iters_df = pd.DataFrame(ith_iters_list)
+        dfo_iters_df = pd.DataFrame(dfo_iters_list)
+        dfo_mod_iters_df = pd.DataFrame(dfo_mod_iters_list)
+        ith_iters = ith_iters_df.mean().tolist()
+        dfo_iters = dfo_iters_df.mean().tolist()
+        dfo_mod_iters = dfo_mod_iters_df.mean().tolist()
+
+        plot_loss(ith_iters, dfo_iters, dfo_mod_iters, i + 1)
+
+
 if __name__ == "__main__":
-    main()
+    # plot_losses()
+    plot_itrations()
+
