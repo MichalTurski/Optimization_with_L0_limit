e499baea35f0b394e9904e7870d0a94f85066584
MichalTurski <michal.antoni.turski@gmail.com>
2020-02-01 09:51:34 +0100

    Minor changes: remove commented code, changed plot title.

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ def plot_loss(hard_thresholding_hist, dfo_hist, dfo_mod_hist, sample_num):
     plt.plot(dfo_mod_hist, label="DFO modified algorithm")
     plt.ylim(ymin=0)
     # plt.yscale("log")
-    plt.title(f'Loss for sample {sample_num}')
+    plt.title(f'Loss for dataset {sample_num}')
     plt.ylabel('Loss', fontsize=12)
     plt.xlabel('Iteration', fontsize=12)
     plt.legend()
@@ -20,15 +20,8 @@ def plot_loss(hard_thresholding_hist, dfo_hist, dfo_mod_hist, sample_num):
 
 
 def main():
-    # X = np.array([[0.1, 0.2], [0.3, 0.3]])
-    # beta = np.array([1, 2])
-    # init_y = np.array([0, 0])
-    # y, hist = iterative_hard_thresholding(X, beta, init_y, 1, 100, 0.06)
-    # plot_loss(hist, np.multiply(2 * hist, 2), 1)
-
     for i, (X, y) in enumerate(dataset_generator()):
         init_beta = np.random.rand(X.shape[1])
-        # init_beta = init_beta / np.linalg.norm(init_beta)
         L0_thresh = int(init_beta.shape[0]/2)
         iter_limit = 1000
         eps = 0.0001
